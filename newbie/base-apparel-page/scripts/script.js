98bee71ce1592c7554d5f43fdb8c23cd7dccdccd
Geovane Galan Fiuza <geovanegalan@gmail.com>
2024-01-06 14:45:35 -0300

    att

--- newbie/base-apparel-page/scripts/script.js
+++ newbie/base-apparel-page/scripts/script.js
@@ -1,11 +1,19 @@
-const emailInput = document.querySelector("#email")
 const btn = document.querySelector("#btn")
-const errorMsg = document.getElementsByClassName(".error-msg")
+const errorMsg = document.querySelector(".error-msg")
+const errorIcon = document.querySelector(".error-icon")
+const emailInput = document.querySelector("#email")
+const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-btn.addEventListener("click", teste)
 
-function teste() {
-    if (emailInput === '') {
-        alert('salve')
+
+btn.addEventListener("click", function () {
+    if (emailInput.value.trim() === '' || !emailInput.value.match(emailValidation)) {
+        errorMsg.innerHTML = "Please provide a valid email"
+        errorIcon.style.display = "inline"
+    } else {
+        errorMsg.innerHTML = ""
     }
-}
+})
+
+
+
