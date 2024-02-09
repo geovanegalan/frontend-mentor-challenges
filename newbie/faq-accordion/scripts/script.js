9d7445d14deddeb011c6b089fce308089e17cff6
Geovane Galan Fiuza <geovanegalan@gmail.com>
2024-02-09 09:53:19 -0300

    att

--- newbie/faq-accordion/scripts/script.js
+++ newbie/faq-accordion/scripts/script.js
@@ -1,9 +1,8 @@
-const buttons = document.querySelectorAll('.btn')
+// const buttons = document.querySelectorAll('.btn')
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const parentSection = button.parentNode
-        const answer = parentSection.querySelector('.answer')
-        answer.hidden = false
-    })
-})
+// buttons.forEach(button => {
+//     button.addEventListener("click", () => {
+       
+//     })
+// })
+
